31a75ba2292639918b4f1f2462fa3141fab1af45
SeoJHeasdw <tjwpghangel@naver.com>
2025-06-17 14:46:18 +0900

    front

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -45,7 +45,6 @@
                 @keyup.enter="handleLogin"
               />
 
-              
               <!-- 로그인 옵션 -->
               <div class="d-flex justify-space-between align-center mb-6">
                 <v-checkbox
